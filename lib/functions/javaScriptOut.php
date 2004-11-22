b775dfb8808dba427991692c8960b35e2adad026
Markus Tacker <m@coderbyheart.de>
2004-11-22 11:18:35 +0000

    - Fix triple slashes
    git-svn-id: file:///data/svn/repos/ausgaben/trunk@55 72ec531f-85e3-0310-9657-ae07b57976a1

--- lib/functions/javaScriptOut.php
+++ lib/functions/javaScriptOut.php
@@ -5,6 +5,7 @@
         $str = str_replace("\n", '', $str);
         $str = str_replace("\r", '', $str);
         $str = addslashes($str);
+        $str = str_replace('\\\\\\', '\\', $str); // Fix double slashes
         return $str;
     }
 
